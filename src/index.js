dc8de7292796730611b5b4bbaac877f7edd63541
Dev <dev@local.local>
2022-06-12 19:43:42 +0200

    three.js init

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import * as THREE from 'three';
 
+const WIDTH = window.innerWidth;
+const HEIGHT = window.innerHeight;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, WIDTH / HEIGHT, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer({antialias: true});
+renderer.setSize( WIDTH, HEIGHT );
+document.body.appendChild( renderer.domElement );
