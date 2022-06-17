1e2d03f8bf6b0e6da4ef79988a209af23f712412
Gal Trapečar <trapecar.gal@gmail.com>
2022-06-18 00:03:37 +0200

    Working for carnation

--- src/l-system.js
+++ src/l-system.js
@@ -1,3 +1,41 @@
-function l_system_init() {
 
+// Currently working for single strand of garden carnation
+// R - root ; S - stem ; M - meristem ; B - bud ; L - leaves
+
+const axiom = 'RMB';
+
+let rules = {
+    M: 'LSM'
+}
+
+function l_system(progressions) {
+    let result_system = axiom;
+    for (let i = 0; i < progressions; i++) {
+        result_system = l_system_progress(result_system);
+    }
+    return result_system;
+}
+
+function l_system_progress(system) {
+    let temp = system;
+    system.split('').forEach((terminal, n) => {
+        if (rules[terminal]) {
+        	temp = temp.substring(0, n) + rules[terminal] + temp.substring(n + 1, temp.length);
+        }
+    })
+    return temp;
+}
+
+console.log(l_system(2));
+
+// Lehmer Random Number Generator
+
+function lehmer16(seed) {
+    seed += 3777035285;
+    let temp = 0;
+    temp = seed * 1245296397;
+    let m1 = (temp >> 16) ^ temp;
+    temp = m1 * 318428617;
+    let m2 = (temp >> 16) ^ temp;
+    return m2;
 }
