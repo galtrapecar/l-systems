2c2c0db07b17537e851c9901f2be0d4b8ce2782c
Gal Trapečar <trapecar.gal@gmail.com>
2022-07-12 22:07:40 +0200

    Added comments

--- src/LSystem.js
+++ src/LSystem.js
@@ -1,5 +1,21 @@
 import { Lehmer16 } from "./Lehmer16";
 
+/* This class implements a system for generating a SD0L-system word from defined parameters.
+ * 
+ * Parameter Summary
+ * 
+ * axiom: string
+ * Specifies the starting sequence of inputs.
+ * 
+ * rules: func: (input: string) => { rules: string[], odds: number[] }
+ * Specifies a function that returns an object with rules for a specific input.
+ * 
+ * IAAdependent?: boolean
+ * Optional. Specifies if current L-system is auxin dependent.
+ * 
+ * IAAdelta?: number
+ * Optional. Specifies the amount auxin changes on each progression.
+ */
 export class LSystem {
     constructor(axiom, rules, seed, IAAdependent, IAAdelta) {
         this.axiom = axiom;
@@ -11,6 +27,13 @@ export class LSystem {
         this.IAA = 0;
     }
 
+    /* Loops a set ammount of times and returns a built L-system.
+     * 
+     * Parameter Summary
+     * 
+     * progressions: int
+     * Specifies number of progressions for an L-system to make.
+     */
     make(progressions) {
         this.clear();
         let system = this.axiom;
@@ -21,6 +44,13 @@ export class LSystem {
         return system;
     }
 
+    /* Loops through a word and replaces characters based on rules defined in the constructor.
+     * 
+     * Parameter Summary
+     * 
+     * system: string
+     * Specifies an L-system string to loop through
+     */
     progress(system) {
         let _system = '';
         system.split('').forEach((input) => {
@@ -47,21 +77,7 @@ export class LSystem {
         return _system;
     }
 
-    _progress(system) {
-        let _system = '';
-        system.split('').forEach((input) => {
-            if (this.rules[input]) {
-                let odds = this.lehmer16.next() % this.rules[input]['odds'].length;
-                let index = this.rules[input]['odds'][odds];
-                let rule = this.rules[input]['rules'][index];
-                _system += rule;
-            } else {
-                _system += input;
-            }
-        })
-        return _system;
-    }
-
+    /* Clears auxin level and RNG seed. */
     clear() {
         this.IAA = 0;
         this.lehmer16 = new Lehmer16(this.seed);
